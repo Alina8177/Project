212d69122323d509b3997e3bae614d9d3ce1a531
Alina8177 <83888270+Alina8177@users.noreply.github.com>
2021-10-25 22:13:12 -0400

    commit changes

--- anim.js
+++ anim.js
@@ -1,90 +1,51 @@
 
 
-var Particle = function (x, y) {
-    this.x = x || Math.round(Math.random() * canvas.width);
-    this.y = y || Math.round(Math.random() * canvas.height);
-    this.r = Math.ceil(Math.random() * config.maxParticleSize);
-    this.c = colorVariation(colorPalette.matter[Math.floor(Math.random() * colorPalette.matter.length)],true );
-    this.s = Math.pow(Math.ceil(Math.random() * config.maxSpeed), .7);
-    this.d = Math.round(Math.random() * 360);
-};
+// var cleanUpArray = function () {
+//     particles = particles.filter((p) => { 
+//       return (p.x > -100 && p.y > -100); 
+//     });
+// };
 
 
-var colorVariation = function (color, returnString) {
-    var r,g,b,a, variation;
-    r = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation/2)) + color.r);
-    g = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation/2)) + color.g);
-    b = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation/2)) + color.b);
-    a = Math.random() + .5;
-    if (returnString) {
-        return "rgba(" + r + "," + g + "," + b + "," + a + ")";
-    } else {
-        return {r,g,b,a};
-    }
-};
+// var initParticles = function (numParticles, x, y) {
+//     for (let i = 0; i < numParticles; i++) {
+//         particles.push(new Particle(x, y));
+//     }
+//     particles.forEach((p) => {
+//         drawParticle(p.x, p.y, p.r, p.c);
+//     });
+// };
 
-var updateParticleModel = function (p) {
-    var a = 180 - (p.d + 90); 
-    p.d > 0 && p.d < 180 ? p.x += p.s * Math.sin(p.d) / Math.sin(p.s) : p.x -= p.s * Math.sin(p.d) / Math.sin(p.s);
-    p.d > 90 && p.d < 270 ? p.y += p.s * Math.sin(a) / Math.sin(p.s) : p.y -= p.s * Math.sin(a) / Math.sin(p.s);
-    return p;
-};
-
-
-var drawParticle = function (x, y, r, c) {
-    ctx.beginPath();
-    ctx.fillStyle = c;
-    ctx.arc(x, y, r, 0, 2*Math.PI, false);
-    ctx.fill();
-    ctx.closePath();
-};
-
-var cleanUpArray = function () {
-    particles = particles.filter((p) => { 
-      return (p.x > -100 && p.y > -100); 
-    });
-};
-
-
-var initParticles = function (numParticles, x, y) {
-    for (let i = 0; i < numParticles; i++) {
-        particles.push(new Particle(x, y));
-    }
-    particles.forEach((p) => {
-        drawParticle(p.x, p.y, p.r, p.c);
-    });
-};
-
-window.requestAnimFrame = (function() {
-  return window.requestAnimationFrame ||
-     window.webkitRequestAnimationFrame ||
-     window.mozRequestAnimationFrame ||
-     function(callback) {
-        window.setTimeout(callback, 1000 / 60);
-     };
-})();
+// window.requestAnimFrame = (function() {
+//   return window.requestAnimationFrame ||
+//      window.webkitRequestAnimationFrame ||
+//      window.mozRequestAnimationFrame ||
+//      function(callback) {
+//         window.setTimeout(callback, 1000 / 60);
+//      };
+// })();
 
 
 
-var frame = function () {
-  drawBg(ctx, colorPalette.bg);
-  particles.map((p) => {
-    return updateParticleModel(p);
-  });
-  particles.forEach((p) => {
-      drawParticle(p.x, p.y, p.r, p.c);
-  });
-  window.requestAnimFrame(frame);
-};
+// var frame = function () {
+//   drawBg(ctx, colorPalette.bg);
+//   particles.map((p) => {
+//     return updateParticleModel(p);
+//   });
+//   particles.forEach((p) => {
+//       drawParticle(p.x, p.y, p.r, p.c);
+//   });
+//   window.requestAnimFrame(frame);
+// };
 
-document.body.addEventListener("click", function (event) {
-    var x = event.clientX,
-        y = event.clientY;
-    cleanUpArray();
-    initParticles(config.particleNumber, x, y);
-});
+// document.body.addEventListener("click", function (event) {
+//     var x = event.clientX,
+//         y = event.clientY;
+//     cleanUpArray();
+//     initParticles(config.particleNumber, x, y);
+// });
 
 
-frame();
+// frame();
 
-initParticles(config.particleNumber);
+// initParticles(config.particleNumber);
